1a9c826c8356e3558420b21df40d825e533c9575
sayandipdutta <sayandip199309@gmail.com>
2024-10-06 23:41:58 +0530

    feat: implement typing area input

--- src/clacktile/__main__.py
+++ src/clacktile/__main__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from typing import Final
 
-from textual.reactive import reactive
 from textual.app import App, ComposeResult, RenderResult
 from textual.containers import Center, Vertical
-from textual.widget import Widget
-from textual.widgets import Header, Static
-
+from textual.message import Message
+from textual.reactive import reactive
+from textual.suggester import Suggester
+from textual.widgets import Header, Input, Static
 
 SAVED_TEXT_PATH: Final[Path] = Path(__file__).parent / "saved"
 
@@ -14,7 +14,7 @@ SAVED_TEXT_PATH: Final[Path] = Path(__file__).parent / "saved"
 def load_text(index: int) -> str:
     files = list(SAVED_TEXT_PATH.glob("*.txt"))
     if not files:
-        return "No files available!"
+        return "No files available"
     index %= len(files)
     return files[index].read_text()
 
@@ -28,18 +28,31 @@ class TextArea(Static, can_focus=True):
     count = reactive(0)
     text = reactive(load_text(0))
 
+    class TextChanged(Message):
+        def __init__(self, text: str) -> None:
+            self.text = text
+            super().__init__()
+
     def render(self) -> RenderResult:
         return self.text
 
     def action_goto_text(self, amount: int) -> None:
         self.count += amount
         self.text = load_text(self.count)
+        self.post_message(self.TextChanged(self.text))
+        StaticSuggester.current_text = self.text
 
 
-# class TypingArea(Widget):
-#     CSS_PATH = "style/typing.tcss"
-#
-#     def compose(self) -> ComposeResult: ...
+class TypingArea(Input):
+    def action_cursor_right(self) -> None:
+        self.cursor_position += 1
+
+
+class StaticSuggester(Suggester):
+    current_text: str = load_text(0)
+
+    async def get_suggestion(self, value: str) -> str | None:
+        return self.current_text
 
 
 class ClacktileApp(App):
@@ -52,7 +65,14 @@ class ClacktileApp(App):
             with Center():
                 yield TextArea(id="text")
             with Center():
-                yield Static("typing", id="input")
+                yield TypingArea(
+                    placeholder="Start typing",
+                    id="input",
+                    suggester=StaticSuggester(),
+                )
+
+    def on_text_area_text_changed(self, message: TextArea.TextChanged) -> None:
+        self.query_one("#input", expect_type=TypingArea).clear()
 
 
 if __name__ == "__main__":
